2e4eb08dec29c878942eb48904eb0d5ee68e45f4
chase coogan <chasecoogan12@gmail.com>
2023-04-20 21:37:30 -0400

    final!!

---
+++ src/PhotoAlbumMain.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+
+import Controller.IController;
+import Controller.Controller;
+import exceptions.IllegalShapeTypeException;
+
+/**
+ * The type Photo album main.
+ */
+public class PhotoAlbumMain {
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   * @throws NoSuchFieldException      the no such field exception
+   * @throws IllegalAccessException    the illegal access exception
+   * @throws IllegalShapeTypeException the illegal shape type exception
+   * @throws IOException               the io exception
+   */
+  public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IllegalShapeTypeException, IOException {
+    String inputFile = null;
+    String viewType = null;
+    String outputFile = null;
+    int xmax = 1000;
+    int ymax = 1000;
+
+    if (args.length < 4) {
+      System.out.println("Insufficient arguments");
+      System.exit(1);
+    }
+
+    for (int i = 0; i < args.length; i++) {
+      switch (args[i]) {
+        case "-in":
+          inputFile = args[++i];
+          break;
+        case "-view":
+          viewType = args[++i];
+          break;
+        case "-v":
+          viewType = args[++i];
+          break;
+        case "-out":
+          outputFile = args[++i];
+          break;
+        default:
+          xmax = Integer.parseInt(args[i]);
+          ymax = Integer.parseInt(args[++i]);
+          break;
+      }
+    }
+    IController contr = new Controller(inputFile, outputFile, viewType, xmax, ymax);
+    contr.run();
+  }
+}
